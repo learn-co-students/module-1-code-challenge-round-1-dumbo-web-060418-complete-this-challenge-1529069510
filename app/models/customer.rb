db34768fb3e6013f469ab474cc0d44d8db9c8d20
CharlieTung <choelestung@gmail.com>
2018-06-15 10:32:03 -0400

    crushed it

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,51 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(full_name)
+    self.all.detect do |f_name|
+      f_name == full_name
+    end
+  end
+
+  def self.find_all_by_first_name(first_name)
+    self.all.select do |get_first_name|#get the first name out of the array of full_name
+      get_first_name == @first_name
+    end
+    got_name = get_first_name #iterate to return all the instances of firstname that are equal to the string arguement
+    got_name.find_all do |f_name|
+      f_name == first_name
+      #can possibly dry this but no time
+    #   binding.pry
+    end
+  end
+
+  def self.all_names
+    self.all
+  end
+
+  def add_review(restaurant, content)
+    Review.new(self, restaurant, content)
+  end
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,28 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+cust1 = Customer.new("char", "tung")
+cust2 = Customer.new("dar", "chu")
+cust3 = Customer.new("dar", "helpme")
+cust4 = Customer.new("dar", "saveI")
+cust5 = Customer.new("google", "answers")
+
+
+
+rest1 = Restaurant.new("techeats")
+rest2 = Restaurant.new("metalfoods")
+rest3 = Restaurant.new("rawore")
+rest4 = Restaurant.new("pickandbones")
+rest5 = Restaurant.new("eletricEggs")
+
+
+rev1 = Review.new(cust1, rest1, "great")
+rev2 = Review.new(cust1, rest2, "great")
+rev3 = Review.new(cust1, rest3, "great")
+rev4 = Review.new(cust2, rest4, "great")
+rev5 = Review.new(cust2, rest5, "great")
+
+
+
+
+binding.pry
