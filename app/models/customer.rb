d2119caeffb45078c3ee08b94384279af1599997
pbomba <paul@paulbomba.com>
2018-06-15 10:31:51 -0400

    Paul

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
+
+#class methods
+
+	def self.all
+		@@all
+		
+	end
+
+	def self.find_by_name(name)
+		#first customer with this name
+		Customer.all.select { | cust | cust.full_name}[0]
+		
+	end
+
+	def self.find_all_by_first_name(name)
+		#all the customers with this 1st name
+		Customer.all.select { |cust| cust.first_name == name }
+	end
+
+	def self.all_names
+#all the various customer names		
+		Customer.all.map { |cust| cust.full_name }
+	end
+
+#instance methods
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content)
+  	# title = "#{self.first_name}'s review of #{restaurant}"
+  	Review.new(self, restaurant, content)
+  end
+
 end
