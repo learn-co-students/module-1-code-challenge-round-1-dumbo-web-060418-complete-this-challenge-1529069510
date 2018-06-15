6355997c2aa4fae8cd11f018934b10635e44013d
jtx007 <jamesjacobthomas7@gmail.com>
2018-06-15 10:29:31 -0400

    cheesed it

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,42 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(fullname)
+    self.all.find do |customer|
+      customer.full_name == fullname
+    end
+  end
+
+  def self.find_all_by_first_name(firstname)
+    self.all.select do |customer|
+      customer.first_name == firstname
+    end
+  end
+
+  def self.all_names
+    self.all.collect do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, content)
+    Review.new(self, restaurant, content)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+james = Customer.new("James", "Thomas")
+arren = Customer.new("Arren", "Alexander")
+prince = Customer.new("Prince", "Wilson")
+james2 = Customer.new("James", "Madison")
+james3 = Customer.new("James", "Bond")
+prince2 = Customer.new("Prince", "Wilson")
+
+
+robertas = Restaurant.new("Roberta's")
+littletongs = Restaurant.new("Little Tong's Noodle Shop")
+
+amazing = Review.new(james, robertas, "amazing")
+damn = Review.new(arren, littletongs, "damn")
+wowzers = Review.new(prince, robertas, "wowzers")
+
+
+binding.pry
